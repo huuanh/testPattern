5c038b9d83c9528a46380a2c94fde1e8afb8a93b
huuanh <huuanh90@gmail.com>
2016-04-26 07:04:41 +0700

    first commit

---
+++ database/migrations/2016_04_25_162538_create_pokemon_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use \App\Model\Entities\Pokemon;
+
+class CreatePokemonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
